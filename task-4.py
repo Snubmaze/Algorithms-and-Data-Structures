ed3326b4771749449c044ca9ed4a093f504620a4
Pavel Polyarus <164698554+Snubmaze@users.noreply.github.com>
2024-09-18 01:43:45 +0300

    Add files via upload

---
+++ task-4.py
@@ -0,0 +1,13 @@
+file_name=input("enter file name: ")
+
+nums=open(file_name,'r').read()
+
+space_ind=nums.index(" ")
+
+a=int(nums[:space_ind])
+b=int(nums[space_ind+1:])
+
+if abs(a) >= 10**9 or abs(b) >= 10**9:
+    print("wrong input : number out of range")
+else:
+    output=open("output.txt",'w').write(str(a+b**2))
